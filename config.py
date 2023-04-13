a009faec9fad92bc7cb86a1a896250b8af66be3f
Mr_Cee <Christie.josh@live.com>
2023-04-13 13:39:23 -0800

    Revert "Added N/E/W/S exit functionality test"
    This reverts commit be23cac2

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
 # Window Settings
-WIN_WIDTH = 800  # Must be Multiple of 32 to look nice
-WIN_HEIGHT = 800  # Must be Multiple of 32 to look nice
+WIN_WIDTH = 800 # Must be Multiple of 32 to look nice
+WIN_HEIGHT = 800 # Must be Multiple of 32 to look nice
 WIN_BG = (17, 102, 24)
 
 # Colors
@@ -14,9 +14,11 @@ FPS = 60
 TERRAIN_LAYER = 1
 PLAYER_LAYER = TERRAIN_LAYER + 1
 
+
 # Sprite Settings
 CHARACTER_TILESIZE = 32
 BORDER_TILESIZE = 32
+
 
 # Player Settings
 PLAYER_SPEED = 3
